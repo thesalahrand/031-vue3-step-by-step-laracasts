4c4943e772f733980f6ba6f9d860a4e553b6a075
Mohammad Salah <thesalahrand@gmail.com>
2023-07-28 16:49:59 +0600

    Ep-6

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,10 +1,24 @@
 export default {
-  template: `<button class="bg-purple-600 hover:bg-purple-800 px-5 py-2.5 text-white rounded mx-2 disabled:cursor-not-allowed" disabled="processing ? true : false">
+  template: `<button 
+    :class="{
+      'bg-purple-600 hover:bg-purple-800': type == 'primary',
+      'bg-green-600 hover:bg-green-800': type == 'success',
+      'bg-orange-600 hover:bg-orange-800': type == 'warning',
+      'bg-red-600 hover:bg-red-800': type == 'danger',
+    }"
+    class="px-5 py-2.5 text-white rounded mx-2 disabled:cursor-not-allowed" 
+    :disabled="processing"
+  >
     <slot/>
   </button>`,
-  data() {
-    return {
-      processing: true,
-    }
+  props: {
+    type: {
+      type: String,
+      default: 'primary',
+    },
+    processing: {
+      type: Boolean,
+      default: false,
+    },
   },
 }
